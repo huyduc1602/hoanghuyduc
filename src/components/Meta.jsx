78b74d539700befe2a37110fd47978b9d50b4c04
Hoàng Huy Đức <56194777+huyduc1602@users.noreply.github.com>
2025-02-11 11:23:51 +0700

    Fix:lỗi comment code

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -9,7 +9,7 @@ const Meta = () => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="title" content="Hoang Huy Duc — Developer">
             <meta name="description" content="Profile website of Hoang Huy Duc">
-            <!-- Open Graph / Facebook -->
+            {/* -- Open Graph / Facebook -- */}
             <meta property="og:type" content="website">
             <meta property="og:title" content="Hoang Huy Duc">
             <meta property="og:description" content="Portfolio page made by Hoang Huy Duc in React.">
@@ -18,7 +18,7 @@ const Meta = () => {
             <meta property="og:image:type" content="image/png">
             <meta property="og:image:width" content="1094">
             <meta property="og:image:height" content="533">
-            <!-- Twitter -->
+            {/* -- Twitter -- */}
             <meta property="twitter:card" content="summary_large_image">
             <meta property="twitter:url" content="https://hoanghuyduc.com/">
             <meta property="twitter:title" content="Hoang Huy Duc">
