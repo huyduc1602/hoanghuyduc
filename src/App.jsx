b38d595b080f8ae0dcfd938a7edea4f2e61656e6
Hoang Huy Duc <huyduc1602@gmail.com>
2025-02-16 21:30:43 +0700

    fix: switch from BrowserRouter to HashRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
@@ -6,7 +6,7 @@ import Meta from "./components/Meta";
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router>
       <Meta />
       <main className='bg-slate-300/20'>
         <Navbar />
