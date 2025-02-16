8e3bc67a7d5d501ad485d6915ac292cd21194cc8
Hoang Huy Duc <huyduc1602@gmail.com>
2025-02-16 21:24:26 +0700

    fix: update miss import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
+import Meta from "./components/Meta";
 
 const App = () => {
   return (
