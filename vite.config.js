aad1afdd9f707ad5dba28e85482fbc37c20e6066
Hoang Huy Duc <huyduc1602@gmail.com>
2025-02-16 21:37:42 +0700

    fix: update routing to use BrowserRouter and add custom 404 handling

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    assetsInclude: ['**/*.glb'],
-    base: '/',
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.glb'],
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        '404': './public/404.html'
+      }
+    }
   }
-  return config
 })
