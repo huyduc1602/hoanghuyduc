edc9f54950799ad25ff12dc1e01e1119ecef6e78
Hoang Huy Duc <huyduc1602@gmail.com>
2025-02-17 16:51:47 +0700

    fix: update skill block width in About component and remove unused styles from globals.css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
 
         <div className='mt-16 flex flex-wrap gap-12'>
           {skills.map((skill) => (
-            <div className='block-container w-20 h-20' key={skill.name}>
+            <div className='block-container w-[5rem] h-20' key={skill.name}>
               <div className='btn-back rounded-xl' />
               <div className='btn-front rounded-xl flex justify-center items-center'>
                 <img
